a21d93cc1a0810448ef74ce670f4a5e205c9cde7
Zanoroy <zanoroy@gmail.com>
2018-01-04 13:26:12 +1030

    Initial commit

---
+++ Source/BlackCyborg/BlackCyborgGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BlackCyborgGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ABlackCyborgGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ABlackCyborgGameMode();
+};
+
+
+
