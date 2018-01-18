f84a5a8ee28e0272e578cc14fb17fbb76c06458f
Zanoroy <zanoroy@gmail.com>
2018-01-18 18:30:17 -0500

    Clean up, complete - NavMesh - Dynamic.

--- Source/BlackCyborg/BlackCyborgHUD.cpp
+++ Source/BlackCyborg/BlackCyborgHUD.cpp
@@ -10,7 +10,7 @@
 ABlackCyborgHUD::ABlackCyborgHUD()
 {
 	// Set the crosshair texture
-	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT("/Game/FirstPerson/Textures/FirstPersonCrosshair"));
+	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT("/Game/Characters/Textures/FirstPersonCrosshair"));
 	CrosshairTex = CrosshairTexObj.Object;
 }
 
