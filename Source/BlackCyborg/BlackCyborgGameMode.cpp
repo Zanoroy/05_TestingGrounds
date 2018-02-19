3073b714f86812a1b192a4bcc0fa4260ab90ebf6
Zanoroy <zanoroy@gmail.com>
2018-02-19 18:08:22 +1030

    Fixed Game mode error
    Also fixed Guard Behavious tree
    
    Guards still flicker when shooting and walking, and then shot while walking.

--- Source/BlackCyborg/BlackCyborgGameMode.cpp
+++ Source/BlackCyborg/BlackCyborgGameMode.cpp
@@ -9,9 +9,9 @@ ABlackCyborgGameMode::ABlackCyborgGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Characters/Main_Character/Behaviour/BP_Main_Character"));
-	DefaultPawnClass = PlayerPawnClassFinder.Class;
+//	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Characters/Main_Character/Behaviour/BP_Main_Character"));
+//	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
-	HUDClass = ABlackCyborgHUD::StaticClass();
+//	HUDClass = ABlackCyborgHUD::StaticClass();
 }
